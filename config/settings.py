f920fd649a04337f0da0376c4ab54bf7488dd14a
ZEELRAJPUT202006 <Zeelrajput2006@gmail.com>
2026-05-06 13:58:25 +0530

    modify settings.py

--- config/settings.py
+++ config/settings.py
@@ -130,11 +130,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zetta_social',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
